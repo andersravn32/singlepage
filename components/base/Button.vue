274a4353b07f4195ca0679f191db64f5bb71d73b
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-01-23 09:19:54 +0100

    Add: Project reset

--- components/base/Button.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <button class="bg-violet-600 rounded px-6 py-2 text-zinc-100">
-        <slot/>
-    </button>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,3 @@
-<script setup>
-import bg from "../assets/img/topo.svg";
-</script>
-
 <template>
-  <section id="hero" :style="{ 'background-image': `url(${bg})` }">
-    <div class="flex flex-col text-center max-w-xl">
-      <h1 class="font-bold text-4xl">En <span class="text-violet-600">hjemmeside</span> er ikke længere blot en hjemmeside.</h1>
-      <p>Hos Singlepage Development udvikler vi speciallavede webløsninger til små og mellemstore virksomheder i Danmark</p>
-    </div>
-  </section>
+  <h1></h1>
 </template>
-
-<style>
-#hero {
-  @apply min-h-screen w-full bg-contain bg-repeat flex items-center justify-center;
-  animation: 60s linear infinite bg;
-}
-
-@keyframes bg{
-    0%{
-        background-position: 0% 0%;
-    }
-
-    100%{
-        background-position: 100% 100%;
-    }
-}
-</style>
